4b8b7405dc5ecd908f9100fa64ee64448651c183
Mike <hallamm@b2b.regn.net>
2022-09-20 10:19:49 +0100

    added relative paths for css in app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const fs=require('fs');
 const express=require('express');
+const path = require('path');
 const app = express();
 const port = 5000;
+
 app.set('view engine', 'ejs');
+app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')))
+app.use('/js', express.static(path.join(_dirname, 'node_modules/bootstrap/dist/js')))
 
 const routes = fs.readdirSync('./routes').filter(file => file.endsWith('.js'));
 routes.forEach(route =>{
