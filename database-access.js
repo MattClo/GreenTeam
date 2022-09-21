189f7de8b5cf041ed657e5874aa5b2fa94663996
Mike <hallamm@b2b.regn.net>
2022-09-21 14:19:46 +0100

    Fixed some issues within database creation and querying. Still have yet to check that database has correct tables created (and indeed, this looks not to be the case currently)

--- database-access.js
+++ database-access.js
@@ -12,7 +12,7 @@ function createTables() {
         username TEXT,
         password TEXT 
     );
-`/*
+
     CREATE TABLE users (
         username TEXT,
         password TEXT,
@@ -29,7 +29,7 @@ function createTables() {
         FOREIGN KEY (username)
         REFERENCES users (username);
     );
-    `;//*/
+    `;
 
     db.run(tables_sql)
     db.close()
@@ -41,25 +41,59 @@ function queryAllUsers() {
     const sql = "SELECT * FROM users"
     const usrs = new Array()
     console.log("begin query")
-    db.all(sql, null, (err, rows) => {
-        rows.forEach((row) => {
-            const x = {
-                id: row.id,
-                username: row.username,
-                password: row.password,
-                fname: row.fname
-            };
-            usrs.push(row);
-        })
+        db.all(sql, null, (err, rows) => {
+            if(undefined !== rows) {
+            rows.forEach((row) => {
+                x = new User(row.username, row.password,
+                    row.fname, row.lname, row.location,
+                    row.role, row.biography)
+                usrs.push(row);
+            })
+        }
     })
 
     return usrs
 }
 
+class Admin {
+    
+}
+
+class User {
+    constructor(uname, pwd, fname, lname,
+        location, role, bio) {
+            this.uname = uname
+            this.pwd = pwd
+            this.fname = fname
+            this.lname = lname
+            this.location = location
+            this.role = role
+            this.bio = bio
+    }
+
+    fullname() {
+        return this.fname + " " + this.lname
+    }
+}
+
+function addUser(user) {
+    const sql = `
+    INSERT INTO users
+    VALUES (${user.uname}, ${user.pwd}, ${user.fname}, ${user.lname},
+        ${user.location}, ${user.role}, ${user.biography})
+    `
+    const db = createDBConnection()
+    db.run(sql)
+    db.close()
+}
+
 function dropDB() {
     const fs = require("fs")
     console.log("Removing db")
-    fs.rm(DB_NAME)
+    const path = "./"+DB_NAME
+    if(fs.existsSync(path)) {
+        fs.unlinkSync(path)
+    }
     console.log("DB Removed")
 }
 
@@ -67,10 +101,10 @@ function createDBConnection() {
     return new sqlite3.Database(DB_NAME, {verbose: true})
 }
 
+test_user = new User("john", "small", "John", "Smith", "London", "Dev", "I have worked for a while")
 
 dropDB()
 createTables()
-console.log("begin print")
-//console.log(queryAllUsers())
-console.log("end print")
+addUser(test_user)
+console.log(queryAllUsers())
 
