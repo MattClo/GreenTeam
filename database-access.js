9ac7893088e3b9a389abaac1055bf5e802dc054b
Mike <hallamm@b2b.regn.net>
2022-09-21 12:24:36 +0100

    added db file to gitignore. began writing some sql and ran into javascript based issues with debugging

--- database-access.js
+++ database-access.js
@@ -1,16 +1,18 @@
-import sqlite3 from "sqlite3";
-import { open } from 'sqlite';
+const sqlite3 = require("sqlite3").verbose();
+const sqlite = require("sqlite");
+const express = require("express");
 
 const DB_NAME = "people.db"
 
 function createTables() {
-    const db = createDBConnection
+    const db = createDBConnection()
+    console.log("created db")
     const tables_sql = `
     CREATE TABLE admin (
         username TEXT,
-        password TEXT,
+        password TEXT 
     );
-
+`/*
     CREATE TABLE users (
         username TEXT,
         password TEXT,
@@ -18,7 +20,7 @@ function createTables() {
         lname TEXT,
         location TEXT,
         role TEXT,
-        biography TEXT,
+        biography TEXT
     );
 
     CREATE TABLE tags (
@@ -27,11 +29,48 @@ function createTables() {
         FOREIGN KEY (username)
         REFERENCES users (username);
     );
-    `;
+    `;//*/
 
-    db.exec(tables_sql)
+    db.run(tables_sql)
+    db.close()
+    console.log("created tables")
+}
+
+function queryAllUsers() {
+    const db = createDBConnection()
+    const sql = "SELECT * FROM users"
+    const usrs = new Array()
+    console.log("begin query")
+    db.all(sql, null, (err, rows) => {
+        rows.forEach((row) => {
+            const x = {
+                id: row.id,
+                username: row.username,
+                password: row.password,
+                fname: row.fname
+            };
+            usrs.push(row);
+        })
+    })
+
+    return usrs
+}
+
+function dropDB() {
+    const fs = require("fs")
+    console.log("Removing db")
+    fs.rm(DB_NAME)
+    console.log("DB Removed")
 }
 
 function createDBConnection() {
-    return new sqlite3.Database(DB_NAME)
+    return new sqlite3.Database(DB_NAME, {verbose: true})
 }
+
+
+dropDB()
+createTables()
+console.log("begin print")
+//console.log(queryAllUsers())
+console.log("end print")
+
