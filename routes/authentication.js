b92994844ff578b44521d5dcb510f3af70ba7be5
Matt Clough <Matthew.Clough@lexisnexisrisk.com>
2022-09-22 12:06:50 +0100

    Updated home design

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,6 +1,23 @@
 module.exports = async (app) =>{
+    const sqlite3 = require('sqlite3').verbose();
+    let db = new sqlite3.Database('people.db');
+    let sql = `select * from people`;
+    let people;
+    await(db.all(sql,[],(err, rows)=>{if(err)throw err; people=rows;}));
+    db.close();
+
     app.post('/authentication',(req,res)=>{
-        req.session.user=req.body.username;
-        res.redirect('/home');
+        people.forEach(person=>{
+            if(person.username==req.body.username){
+                req.session.name=person.forename+" "+person.surname;
+                req.session.user=req.body.username;
+            }
+        });
+        if(req.session.user!=null){
+            res.redirect('/home');
+        }
+        else{
+            res.redirect('/login');
+        }
     })
 }
