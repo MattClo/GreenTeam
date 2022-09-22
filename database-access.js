327bf6a6b45304ee6936c58efff2bb7a5b9eedf3
Mike <hallamm@b2b.regn.net>
2022-09-22 10:03:22 +0100

    we can now create the tables - we have yet to test user insertion

--- database-access.js
+++ database-access.js
@@ -2,16 +2,50 @@ const sqlite3 = require("sqlite3").verbose();
 const sqlite = require("sqlite");
 const express = require("express");
 
+
+class User {
+    constructor(uname, pwd, fname, lname,
+        location, role, bio) {
+            this.uname = uname
+            this.pwd = pwd
+            this.fname = fname
+            this.lname = lname
+            this.location = location
+            this.role = role
+            this.bio = bio
+    }
+
+    fullname() {
+        return this.fname + " " + this.lname
+    }
+}
+
 const IN_MEM = ":memory:"
 const DB_NAME = "people.db"
-const db = new sqlite3.Database(IN_MEM, sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        console.log("Getting error " + err);
-        exit(1);
-    }
-});
+const db = new sqlite3.Database(DB_NAME)
 
-function dropDB() {
+const tables_sql = [
+    "CREATE TABLE users(username TEXT, password TEXT, fname TEXT, lname TEXT, location TEXT, role TEXT, biography TEXT);",
+    "CREATE TABLE admin(username TEXT, password TEXT);",
+    "CREATE TABLE tags(username TEXT, interest TEXT);"
+];
+
+for(s of tables_sql) {
+    db.run(s)
+}
+
+db.close()
+
+user = new User("john", "small", "John", "Smith", "London", "Dev", "I have worked for a while")
+
+const db1 = new sqlite3.Database(DB_NAME)
+const q = "INSERT INTO users(?, ?, ?, ?, ?, ?, ?)"
+const query = db.prepare(q)
+query.run(user.uname, user.pwd, user.fname, user.lname, 
+    user.location, user.role, user.bio)
+query.finalize()
+
+/*function dropDB() {
     const fs = require("fs")
     fs.unlinkSync(DB_NAME)
 }
@@ -88,4 +122,4 @@ createTables()
 //printTables()
 addUser(test_user)
 //console.log(queryAllUsers())
-
+*/
