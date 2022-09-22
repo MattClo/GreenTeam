1d0d9998d3d473c66d253adb6d3cb9151dff6a5b
Mike <hallamm@b2b.regn.net>
2022-09-22 09:21:47 +0100

    trying to access the database still - no luck so far

--- database-access.js
+++ database-access.js
@@ -2,8 +2,23 @@ const sqlite3 = require("sqlite3").verbose();
 const sqlite = require("sqlite");
 const express = require("express");
 
+const IN_MEM = ":memory:"
 const DB_NAME = "people.db"
-const db = createDBConnection()
+const db = new sqlite3.Database(IN_MEM, sqlite3.OPEN_READWRITE, (err) => {
+    if (err && err.code == "SQLITE_CANTOPEN") {
+        createDatabase();
+        return;
+        } else if (err) {
+            console.log("Getting error " + err);
+            exit(1);
+    }
+    runQueries(db);
+});
+
+function dropDB() {
+    const fs = require("fs")
+    fs.unlinkSync(DB_NAME)
+}
 
 function createTables() {
     const tables_sql = [
@@ -12,32 +27,26 @@ function createTables() {
     "CREATE TABLE tags(username TEXT, interest TEXT)"
     ];
 
-    db.open()
-    for(s of tables_sql) {
-        db.exec(s)
-    }
-    db.close()
+    db.serialize(function () {
+        for(s of tables_sql) {
+            db.run(s)
+        }
+    })
+    //db.close()
 
     console.log("created tables")
 }
 
-function queryAllUsers() {
-    const db = createDBConnection()
-    const sql = "SELECT * FROM users"
-    const usrs = new Array()
-    console.log("begin query")
-        db.all(sql, null, (err, rows) => {
-            if(undefined !== rows) {
-            rows.forEach((row) => {
-                x = new User(row.username, row.password,
-                    row.fname, row.lname, row.location,
-                    row.role, row.biography)
-                usrs.push(row);
-            })
-        }
-    })
+function printTables() {
+    db.serialize(function () {
+        db.each("select name from sqlite_master where type='table'", function (err, table) {
+            console.log(table);
+        });
+    });
+}
 
-    return usrs
+function queryAllUsers() {
+    throw "not implemented"
 }
 
 class Admin {
@@ -65,58 +74,22 @@ class User {
 }
 
 function addMin(admin) {
-    const sql = `
-    INSERT INTO admin
-    VALUES (${admin.uname}, ${admin.pwd})
-    `
-    execDB(sql)
+    throw "not implemented"
 }
 
 function addUser(user) {
-    const sql = `
-    INSERT INTO users
-    VALUES (${user.uname}, ${user.pwd}, ${user.fname}, ${user.lname},
-        ${user.location}, ${user.role}, ${user.biography})
-    `
-    execDB(sql)
-}
-
-function execDB(sql) {
-    const db = createDBConnection()
-    db.serialize(function () {
-        db.run(sql)
-    })
-    db.close()
-}
-
-function dropDB() {
-    const fs = require("fs")
-    console.log("Removing db")
-    const path = "./"+DB_NAME
-    if(fs.existsSync(path)) {
-        fs.unlinkSync(path)
-    }
-    console.log("DB Removed")
-}
-
-function createDBConnection() {
-    return new sqlite3.Database(DB_NAME, {verbose: true})
-}
-
-function printTables() {
-    const db = createDBConnection()
-    db.serialize(function () {
-        db.all("select name from sqlite_master where type='table'", function (err, tables) {
-            console.log(tables);
-        });
-    });
+    const q = "INSERT INTO users(?, ?, ?, ?, ?, ?, ?)"
+    const query = db.prepare(q)
+    query.run(user.uname, user.pwd, user.fname, user.lname, 
+        user.location, user.role, user.bio)
+    query.finalize()
 }
 
 test_user = new User("john", "small", "John", "Smith", "London", "Dev", "I have worked for a while")
 
-dropDB()
+//dropDB()
 createTables()
 //printTables()
-//addUser(test_user)
+addUser(test_user)
 //console.log(queryAllUsers())
 
